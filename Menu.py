639835a76e11e3f59c2fc18028c77e6f184e3aa8
JuanPabloCifuentes <juan.pi1456@gmail.com>
2025-03-20 07:32:26 -0500

    Initial commit

---
+++ Menu.py
@@ -0,0 +1,12 @@
+menu_principal = """
+******Bienvenidos a la pan loca******
+1. Soy cliente
+2. Soy Administrador
+3. Para salir 
+********Gracias por la antención prestada********
+"""
+menu_cliente = """ 
+******Hola, Productos disponibles******
+1. Croasan 
+2. Pan de la Abuela 
+"""
